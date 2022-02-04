6a16bd7602b2eb08b97386fd306575392f82b377
Jesus <jesusbalandra@outlook.com>
2022-02-03 20:39:56 -0700

    added signup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./apiRoutes');
 const { renderHomePage, loginView, signupView } = require('../controllers/userController')
-
+const { getAllTodos } = require('../controllers/todoController')
 router.get('/', renderHomePage);
+router.get('/todos', getAllTodos);
 router.use('/api', apiRoutes);
 router.get('/login', loginView);
 router.get('/signup', signupView);
